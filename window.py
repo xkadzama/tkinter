d8f683782a57a4f8622ee27cf780b16b5b2c825a
xkadzama <ya.amerika-2015@yandex.ru>
2020-08-14 23:11:55 +0300

    up

---
+++ window.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+def print_text():
+    print('Ты чмо')
+
+
+root = Tk()
+root.resizable(width = False, height = False)
+root.geometry( '400x300')
+root.title('AEIOU')
+# root['bg'] = 'green'
+
+button = Button(text='Send message', command=print_text)
+button.pack(side=RIGHT)
+root.mainloop()
